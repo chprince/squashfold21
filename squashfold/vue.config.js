b69627ff353c870c8a3790ef671fbc039086ef42
Charlie Prince <charlie.prince@reasondigital.com>
2021-10-12 18:33:48 +0100

    Fix links for gh pages

--- squashfold/vue.config.js
+++ squashfold/vue.config.js
@@ -4,6 +4,12 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 
+ module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/squashfold/' // note the trailing slash
+      : '/',
+  };
+
 // module.exports = {
 //     css: {
 //       loaderOptions: {
